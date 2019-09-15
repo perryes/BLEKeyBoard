36b3f73f999400fdac85d431a9e552c7e9ecbcdd
perry <perryes@hotmail.com>
2019-09-15 17:24:33 +0800

    add changes

---
+++ MacKeyboard/MacKeyboard/DeviceCellView.h
@@ -0,0 +1,13 @@
+//
+//  DeviceCellView.h
+//  MacKeyboard
+//
+//  Created by liuyang on 2017/11/1.
+//  Copyright © 2017年 liuyang. All rights reserved.
+//
+
+#ifndef DeviceCellView_h
+#define DeviceCellView_h
+
+
+#endif /* DeviceCellView_h */
---
+++ MacKeyboard/MacKeyboard/DeviceCellView.m
@@ -0,0 +1,9 @@
+//
+//  DeviceCellView.m
+//  MacKeyboard
+//
+//  Created by liuyang on 2017/11/1.
+//  Copyright © 2017年 liuyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
